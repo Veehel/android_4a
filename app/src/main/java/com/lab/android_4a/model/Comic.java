889248b396f6e3aedb779249d29a266c19d90db8
Veehel <veeehel@gmail.com>
2019-10-07 11:17:32 +0200

    added xkcd API

---
+++ app/src/main/java/com/lab/android_4a/model/Comic.java
@@ -0,0 +1,117 @@
+package com.lab.android_4a.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Comic {
+
+    @SerializedName("num")
+    private Integer num;
+
+    @SerializedName("day")
+    private String day;
+
+    @SerializedName("month")
+    private String month;
+
+    @SerializedName("year")
+    private String year;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("safe_title")
+    private String safe_title;
+
+    @SerializedName("transcript")
+    private String transcript;
+
+    @SerializedName("alt")
+    private String alt;
+
+    @SerializedName("img")
+    private String img;
+
+    public Comic(Integer num, String day, String month, String year, String title, String safe_title, String transcript, String alt, String img) {
+        this.num = num;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.title = title;
+        this.safe_title = safe_title;
+        this.transcript = transcript;
+        this.alt = alt;
+        this.img = img;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSafe_title() {
+        return safe_title;
+    }
+
+    public void setSafe_title(String safe_title) {
+        this.safe_title = safe_title;
+    }
+
+    public String getTranscript() {
+        return transcript;
+    }
+
+    public void setTranscript(String transcript) {
+        this.transcript = transcript;
+    }
+
+    public String getAlt() {
+        return alt;
+    }
+
+    public void setAlt(String alt) {
+        this.alt = alt;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
--- app/src/main/java/com/lab/android_4a/network/GetDataService.java
+++ app/src/main/java/com/lab/android_4a/network/GetDataService.java
@@ -1,14 +1,25 @@
 package com.lab.android_4a.network;
 
+import com.lab.android_4a.model.Comic;
 import com.lab.android_4a.model.RetroPhoto;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 import retrofit2.http.GET;
 
 public interface GetDataService {
 
+    public static final String ENDPOINT = "http://xkcd.com";
+
     @GET("/photos")
     Call<List<RetroPhoto>> getAllPhotos();
+
+    @GET("/info.0.json")
+    Call<List<Comic>> getCurrent();
+
+    @GET("/{xkcd_number}/info.0.json")
+    Call<List<Comic>> getComic(@Path("xkcd_number") int xkcd_number);
 }
